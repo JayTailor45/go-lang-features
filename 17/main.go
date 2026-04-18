f631376127428a9b66ab67c0814dd6ddea792a88
JayTailor45 <tailorj64@gmail.com>
2026-04-18 14:23:29 +0530

    refactor code in separate routers file

--- 17/main.go
+++ 17/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 	"jaytailor.com/event-management/db"
-	"jaytailor.com/event-management/models"
+	"jaytailor.com/event-management/routes"
 )
 
 func main() {
@@ -14,52 +11,7 @@ func main() {
 
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
-}
-
-func getEvents(ctx *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events"})
-		return
-	}
-	ctx.JSON(http.StatusOK, events)
-}
-
-func getEvent(ctx *gin.Context) {
-	eventId, err := strconv.ParseInt(ctx.Params.ByName("id"), 10, 64)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch the event"})
-		return
-	}
-	event, err := models.GetEventById(eventId)
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{"message": "Could not find the event"})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, event)
-}
-
-func createEvent(ctx *gin.Context) {
-	var event models.Event
-	err := ctx.ShouldBindJSON(&event)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request"})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create an event"})
-		return
-	}
-	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
