4217ce8d4a7e0b540d6dbd3bdaec9132f86be5d0
JayTailor45 <tailorj64@gmail.com>
2026-04-18 20:24:52 +0530

    store hashed password

--- 17/models/user.go
+++ 17/models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "jaytailor.com/event-management/db"
+import (
+	"jaytailor.com/event-management/db"
+	"jaytailor.com/event-management/utils"
+)
 
 type User struct {
 	ID       int64
@@ -16,7 +19,12 @@ func (u *User) Save() error {
 	}
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
