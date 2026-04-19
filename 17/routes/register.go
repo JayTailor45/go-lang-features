41573285aab124d5678fb44392efbb3594319f4f
JayTailor45 <tailorj64@gmail.com>
2026-04-19 11:54:03 +0530

    register user to event

---
+++ 17/routes/register.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"jaytailor.com/event-management/models"
+)
+
+func registerForEvent(ctx *gin.Context) {
+	userId := ctx.GetInt64("userId")
+	eventId, err := strconv.ParseInt(ctx.Params.ByName("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch the event"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get event"})
+		return
+	}
+
+	err = event.Register(userId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register user to event"})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"message": "User successfully registered"})
+}
+
+// func cancelRegistration(ctx *gin.Context) {}
